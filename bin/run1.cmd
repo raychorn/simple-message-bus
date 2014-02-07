b6e84ca5a743de809e9a924b72476b43bd39ccec
raychorn <raychorn@gmail.com>
2014-02-07 16:24:49 +0000

    4th commit

--- bin/run1.cmd
+++ bin/run1.cmd
@@ -2,7 +2,10 @@
 cls
 echo BEGIN:
 
-"simple-message-bus.exe" -i "message_box1" -o "message_box2"
+REM "simple-message-bus.exe" -i "message_box1" -o "message_box2"
+
+"simple-message-bus.exe" -i "message_box1" -o "127.0.0.1:55555" -r -l "127.0.0.1:50555"
+
 
 exit
 
--- bin/run2.cmd
+++ bin/run2.cmd
@@ -2,7 +2,9 @@
 cls
 echo BEGIN:
 
-"simple-message-bus.exe" -i "message_box2" -o "message_box3"
+REM "simple-message-bus.exe" -i "message_box2" -o "message_box3"
+
+"simple-message-bus.exe" -i "127.0.0.1:55555" -o "message_box2"
 
 exit
 
